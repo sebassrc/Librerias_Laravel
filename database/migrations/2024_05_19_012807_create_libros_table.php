83bd24b31f49f3e0d96c06ff3f492756ec820ac2
Sebastian Rodriguez Camilo <segarodriguezcamilo@gmail.com>
2024-05-18 20:30:16 -0500

    Migration libros

---
+++ database/migrations/2024_05_19_012807_create_libros_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('autor');
+            $table->string('isbn')->unique();
+            $table->decimal('precio', 8, 2);
+            $table->integer('stock');
+            $table->unsignedBigInteger('categoria_id');
+            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+};
