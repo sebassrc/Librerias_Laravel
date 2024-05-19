9d3e1b6ab2ca3dc71ecc8163be7309fa5b02b6b5
Sebastian Rodriguez Camilo <segarodriguezcamilo@gmail.com>
2024-05-18 20:31:31 -0500

    Migration ventas

---
+++ database/migrations/2024_05_19_013052_create_ventas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('libro_id');
+            $table->unsignedBigInteger('cliente_id');
+            $table->integer('cantidad');
+            $table->date('fecha_venta');
+            $table->decimal('total', 8, 2);
+            $table->foreign('libro_id')->references('id')->on('libros')->onDelete('cascade');
+            $table->foreign('cliente_id')->references('id')->on('clientes')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ventas');
+    }
+};
