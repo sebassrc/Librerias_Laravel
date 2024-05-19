24b7cf9fb6d90f479e347154456a83af4c309ccf
Sebastian Rodriguez Camilo <segarodriguezcamilo@gmail.com>
2024-05-18 20:47:44 -0500

    Model evento

---
+++ app/Models/Evento.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Evento extends Model
+{
+    use HasFactory;
+    protected $table = "eventos";
+    protected $primaryKey = "id";
+    public $timestamps = false;
+}
