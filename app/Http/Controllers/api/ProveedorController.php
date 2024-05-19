d9993df39007625d0c05ad3aafe3abb4fb4f8743
Sebastian Rodriguez Camilo <segarodriguezcamilo@gmail.com>
2024-05-18 21:18:06 -0500

    Controller api proveedor

---
+++ app/Http/Controllers/api/ProveedorController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Proveedor;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ProveedorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $proveedores = DB::table('proveedores')->get();
+        return json_encode(['proveedores' => $proveedores]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $proveedor = new Proveedor();
+        $proveedor->nombre = $request->nombre;
+        $proveedor->contacto = $request->contacto;
+        $proveedor->telefono = $request->telefono;
+        $proveedor->direccion = $request->direccion;
+        $proveedor->save();
+        return json_encode(['proveedor' => $proveedor]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $proveedor = Proveedor::find($id);
+        return json_encode(['proveedor' => $proveedor]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $proveedor = Proveedor::find($id);
+        $proveedor->nombre = $request->nombre;
+        $proveedor->contacto = $request->contacto;
+        $proveedor->telefono = $request->telefono;
+        $proveedor->direccion = $request->direccion;
+        $proveedor->save();
+        return json_encode(['proveedor' => $proveedor]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $proveedor = Proveedor::find($id);
+        $proveedor->delete();
+        $proveedores = DB::table('proveedores')->get();
+        return json_encode(['proveedores' => $proveedores, 'success' => true]);
+    }
+}
